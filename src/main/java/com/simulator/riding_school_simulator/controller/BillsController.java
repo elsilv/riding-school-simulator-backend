1b64d3b518e75625d968a52098b6537a95e3c116
elsilv <elsi.silvennoinen@gmail.com>
2025-02-05 13:55:29 +0200

    Add 'buy bill' feature

--- src/main/java/com/simulator/riding_school_simulator/controller/BillsController.java
+++ src/main/java/com/simulator/riding_school_simulator/controller/BillsController.java
@@ -1,12 +1,9 @@
 package com.simulator.riding_school_simulator.controller;
 
-
 import com.simulator.riding_school_simulator.model.Bills;
 import com.simulator.riding_school_simulator.service.BillsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +22,11 @@ public class BillsController {
     @GetMapping("/unpaid")
     public List<Bills> getUnpaidBills() {
         return billsService.getAllUnpaidBills();
+    }
+
+    @PutMapping("/{id}/pay")
+    public String payBill(@PathVariable Long id) {
+        billsService.payBill(id);
+        return "Bill paid successfully!";
     }
 }
--- src/main/java/com/simulator/riding_school_simulator/service/BillsService.java
+++ src/main/java/com/simulator/riding_school_simulator/service/BillsService.java
@@ -1,12 +1,16 @@
 package com.simulator.riding_school_simulator.service;
 
+import com.simulator.riding_school_simulator.model.Balance;
 import com.simulator.riding_school_simulator.model.Bills;
+import com.simulator.riding_school_simulator.model.Transaction;
 import com.simulator.riding_school_simulator.repository.BalanceRepository;
 import com.simulator.riding_school_simulator.repository.BillsRepository;
 import com.simulator.riding_school_simulator.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,5 +37,27 @@ public class BillsService {
 
     public List<Bills> getAllUnpaidBills() {
         return billsRepository.findByPaidFalse();
+    }
+
+    @Transactional
+    public void payBill(Long billId) {
+        Bills bill = getBillsById(billId);
+
+        if (bill.isPaid()) {
+            throw new RuntimeException("Bill is already paid");
+        }
+
+        Transaction transaction = new Transaction();
+        transaction.setType("BILL_PAYMENT");
+        transaction.setAmount(bill.getAmount().negate());
+        transaction.setDate(new Date());
+        transactionRepository.save(transaction);
+
+        Balance balance = balanceRepository.findById(1L).orElseThrow();
+        balance.setAmount(balance.getAmount().subtract(bill.getAmount()));
+        balanceRepository.save(balance);
+
+        bill.setPaid(true);
+        billsRepository.save(bill);
     }
 }
